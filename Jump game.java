d48c26f8c5fefa07e1f44e6e6bb86dbc7c6d8398
Sagar Patel <sampatel3636@gmail.com>
2019-03-23 14:53:03 -0500

    Code accepted
    Runtime : 3ms
    Complexity : O(n)

---
+++ Jump game.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length <=1)
+            return true;
+        
+        int max = nums[0];
+        
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(max<=i && nums[i] == 0)
+                return false;
+            if(i + nums[i]>max)
+                max = i+nums[i];
+            
+            if(max >= nums.length-1)
+                return true;
+        }
+        
+        return false;
+    }
+}
